21bd1c05d509fe7a183cce2e30d5001434c77fbb
fildakrapa <fi.lla@seznam.cz>
2022-05-14 17:37:03 +0200

    descriptive comment for changes

---
+++ Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOptions : MonoBehaviour
+{
+//	public void PlayGame () {
+//		SceneManager.LoadScene (2);
+//	}
+
+	public void PlayGame () {
+		SceneManager.LoadScene (1);
+	}
+
+	public void MainMenu () {
+		SceneManager.LoadScene (0);
+	}
+
+    public void Controls () {
+		SceneManager.LoadScene (4);
+	}
+
+	public void Track01 () {
+		SceneManager.LoadScene (2);
+	}
+
+    public void Track02 () {
+		SceneManager.LoadScene (3);
+	}
+
+	public void Quit () {
+		Application.Quit();
+	}
+
+
+}
